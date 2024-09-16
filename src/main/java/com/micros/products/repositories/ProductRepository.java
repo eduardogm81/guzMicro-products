12e91a7c9100620ac41ab7cff01929ab948dd97c
edu <eduardogm81@gmail.com>
2024-09-16 20:14:46 +0200

    Asignación dinámica de puertos

--- src/main/java/com/micros/products/repositories/ProductRepository.java
+++ src/main/java/com/micros/products/repositories/ProductRepository.java
@@ -1,7 +1,6 @@
 package com.micros.products.repositories;
 
 import com.micros.products.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
